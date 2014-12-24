658b18ba2365d7f6e005df18d905abc67245c83d
Dan Borthwick <dan.borthwick@king.com>
2014-12-24 13:41:17 +0000

    Group fields and methods together

--- src/main/java/scenes/SceneClassGenerator.java
+++ src/main/java/scenes/SceneClassGenerator.java
@@ -1,5 +1,6 @@
 package scenes;
 
+import cpp.ASTCppField;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -9,11 +10,18 @@ import uk.co.badgersinfoil.metaas.dom.ASCompilationUnit;
 import uk.co.badgersinfoil.metaas.dom.ASField;
 import uk.co.badgersinfoil.metaas.dom.ASMethod;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static uk.co.badgersinfoil.metaas.dom.Visibility.PROTECTED;
 import static uk.co.badgersinfoil.metaas.dom.Visibility.PUBLIC;
 
 public class SceneClassGenerator {
     private final ActionScriptProject project;
+
+    public static final String SCENE_OBJECT_QNAME = "com.king.flash.spaceland.scene.SceneObject";
 
     public SceneClassGenerator(ActionScriptProject project) {
         this.project = project;
@@ -24,23 +32,46 @@ public class SceneClassGenerator {
         ASClassType clazz = (ASClassType) unit.getType();
 
         ASMethod constructor = clazz.newMethod(clazz.getName(), PUBLIC, null);
-        constructor.addParam("rootObject", "engine.SceneObject");
+        constructor.addParam("rootObject", SCENE_OBJECT_QNAME);
 
+        List<String> names = namedNodeNamesFromScene(document);
+        Map<String, ASField> fieldByName = new HashMap<String, ASField>();
+
+        for (String name : names) {
+            ASField field = addField(clazz, name, constructor);
+            fieldByName.put(name, field);
+        }
+
+        for (String name : names) {
+            addGetMethod(clazz, name, fieldByName.get(name));
+        }
+    }
+
+    private List<String> namedNodeNamesFromScene(Document document) {
         // Use XPath or similar to find named nodes?
         NodeList sceneObjects = document.getElementsByTagName("SceneObject");
+        List<String> names = new ArrayList<String>();
         for (int i=0; i < sceneObjects.getLength(); i++)
         {
             Node sceneObject = sceneObjects.item(i);
             String name = nameAttribute(sceneObject);
             if (name != null)
             {
-                String capitalizedName = capitalize(name);
-                ASField field = clazz.newField("_so" + capitalizedName, PROTECTED, "engine.ButtonSceneObject");
-                clazz.newMethod("get" + capitalizedName, PUBLIC, field.getType()).newReturn(field.getName());
-                constructor.addStmt(field.getName() + " = rootObject.find(\"" + name + "\")");
+                names.add(name);
             }
         }
+        return names;
+    }
 
+    private ASField addField(ASClassType clazz, String name, ASMethod constructor) {
+        String capitalizedName = capitalize(name);
+        ASField field = clazz.newField("_so" + capitalizedName, PROTECTED, SCENE_OBJECT_QNAME);
+        constructor.addStmt(field.getName() + " = rootObject.find(\"" + name + "\")");
+        return field;
+    }
+
+    private void addGetMethod(ASClassType clazz, String name, ASField field) {
+        clazz.newMethod("get" + capitalize(name), PUBLIC, field.getType()).newReturn(field.getName());
     }
 
     private static String nameAttribute(Node node)
